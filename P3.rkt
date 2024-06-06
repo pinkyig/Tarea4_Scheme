bf90640e023b44d15a43c19d33f27bc995340289
Alejandro Rojo <pinkyig@MacBook-Pro-de-Alejandro.local>
2024-06-06 08:06:33 -0400

    [ADD] Se agrego funcion de recursion simple

---
+++ P3.rkt
@@ -0,0 +1,33 @@
+#lang scheme
+
+
+
+; Función auxiliar para calcular el dígito descifrado de una fila usando una función dada
+(define calcular_digito
+
+  (lambda (fila funcion)
+    (let* ((m (length fila)))
+      (modulo(floor(/ (apply + (map funcion fila)) m))10))
+
+    )
+  )
+
+; Función para descifrar los mensajes usando recursión simple o cola??? XDD
+(define (descifrar_simple mensajes funciones)
+  (if (null? mensajes)
+      '()
+      (cons (calcular_digito (car mensajes) (car funciones))
+            (descifrar_simple (cdr mensajes) (cdr funciones)))))
+
+; Ejemplos de uso
+(display (descifrar_simple '((5 2 3) (3 2 4 2 25)) (list (lambda (x) (* x 5)) (lambda (x) (/ (* x 2) (+ x 2))))))
+
+
+
+
+
+
+
+
+
+
