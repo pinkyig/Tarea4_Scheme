1034923d354a65f1ca09d295c2b1030c56beff6b
Alejandro Rojo <pinkyig@MacBook-Pro-de-Alejandro.local>
2024-06-06 08:56:48 -0400

    [ADD] Se agrego la funcion descifrar_cola

--- P3.rkt
+++ P3.rkt
@@ -14,13 +14,43 @@
 
 ; Función para descifrar los mensajes usando recursión simple o cola??? XDD
 (define (descifrar_simple mensajes funciones)
+  
   (if (null? mensajes)
+      
       '()
+      
       (cons (calcular_digito (car mensajes) (car funciones))
-            (descifrar_simple (cdr mensajes) (cdr funciones)))))
+            
+            (descifrar_simple (cdr mensajes) (cdr funciones)))
+
+      )
+
+  )
+
+
+
+(define (descifrar_cola_aux mensajes funciones resultado)
+
+  (if (null? mensajes)
+      
+      (reverse resultado)
+
+      (descifrar_cola_aux (cdr mensajes) (cdr funciones) (cons (calcular_digito (car mensajes) (car funciones)) resultado))
+
+      )
+  )
+
+
+(define (descifrar_cola mensajes funciones)
+  (descifrar_cola_aux mensajes funciones '())
+  )
 
 ; Ejemplos de uso
 (display (descifrar_simple '((5 2 3) (3 2 4 2 25)) (list (lambda (x) (* x 5)) (lambda (x) (/ (* x 2) (+ x 2))))))
+(newline)
+(descifrar_cola '((5 2 3) (3 2 4 2 25)) (list (lambda (x) (* x 5)) (lambda (x) (/ (* x 2) (+ x 2)))))
+
+
 
 
 
