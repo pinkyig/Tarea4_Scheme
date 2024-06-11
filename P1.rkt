ea8e75a068c22ab57503d2e328e6912b4e8ed3d8
Alejandro Rojo <pinkyig@MacBook-Pro-de-Alejandro.local>
2024-06-11 16:19:24 -0400

    [UPDATE] cambios en nombres de loop e identaciones

--- P1.rkt
+++ P1.rkt
@@ -7,17 +7,24 @@
      '()
      
      ;; Si no se cumple el if
-     (let loop ((resto (cdr lista)) (menor (car lista)))
+     (let iteracion ((resto (cdr lista)) (menor (car lista)))
         (if (null? resto)
+            ;;Si el resto de la lista  esta vacio sigifica que se encontro el planeta con menor liberacion por que se se hace un car para obetener el nombre 
             (list (car menor))
+            
             (let* ((siguiente (car resto))
                    (nombre (car siguiente))
                    (porcentaje (cadr siguiente))
                    (menor-nombre (car menor))
                    (menor-porcentaje (cadr menor)))
+              
               (if (< porcentaje menor-porcentaje)
-                  (loop (cdr resto) siguiente)
-                  (loop (cdr resto) menor)))))))
+                  (iteracion (cdr resto) siguiente)
+                  (iteracion (cdr resto) menor))
+              
+              )))
+     )
+  )
   
 
 ;; Ejemplos de uso:
