5aae31c36d43de4ee4d4b618399a75b9bc246c39
Alejandro Rojo <pinkyig@MacBook-Pro-de-Alejandro.local>
2024-06-13 14:03:03 -0400

    [Update] Se cambiaron nombres de funciones para ser mas entendibles y comentarios a estas

--- P4.rkt
+++ P4.rkt
@@ -1,27 +1,37 @@
 #lang scheme
 
 (define (stratatree seq tree)
-  (define (navigate seq subtree)
+  <
+  (define (recorrido seq subArbol)
+    ;; Función auxiliar para recorrer el árbol siguiendo la secuencia dada.
+    ;; seq : Lista de símbolos que representan direcciones.
+    ;; subArbol : Subárbol actual en la recursión.
     (if (null? seq)
-        subtree
+        subArbol
         (let ((direction (car seq))
-              (children (cdr subtree)))
-          (if (null? children)
+              (hijo (cdr subArbol)))
+          (if (null? hijo)
               #f
-              (let ((next-subtree
-                     (find-child direction children)))
-                (if next-subtree
-                    (navigate (cdr seq) next-subtree)
-                    #f))))))
+              (let ((sigArbol
+                     (busquedaHijo direction hijo)))
+                (if sigArbol
+                    (recorrido (cdr seq) sigArbol)
+                    #f)))
+          )
+        )
+    )
   
-  (define (find-child direction children)
-    (cond ((null? children) #f)
-          ((and (pair? (car children)) (eq? (caar children) direction))
-           (car children))
-          (else (find-child direction (cdr children)))))
+  (define (busquedaHijo direction hijo)
+    ;; Función auxiliar para buscar un hijo en un subárbol que coincida con la dirección dada.
+    ;; direction : Dirección actual en la secuencia.
+    ;; hijo : Lista de hijos del subárbol actual.
+    (cond ((null? hijo) #f)
+          ((and (pair? (car hijo)) (eq? (caar hijo) direction))
+           (car hijo))
+          (else (busquedaHijo direction (cdr hijo)))))
   
   (if (and (not (null? seq)) (eq? (car seq) 'S))
-      (navigate (cdr seq) tree)
+      (recorrido (cdr seq) tree)
       #f))
 
 ;; Ejemplo de uso:
@@ -33,8 +43,6 @@
 
 (display (stratatree '(S L R U) '(S (U (D (" Adv Weapon")) (L ("HellBomb"))(U ("Suministros"))) (L (D (" Orbital Strike")) (R (R (" Air Strike")))))))
 (newline)
-
-(display (stratatree '(S U) '(S (U (D (" Adv Weapon")) (L ("HellBomb")) (U ("Suministros"))) (L (D (" Orbital Strike")) (R (R (" Air Strike")))))))
 
 
 
