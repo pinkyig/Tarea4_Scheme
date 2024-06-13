6e4dbcb6252fc5c2c650ab9d2cbbbc83da9464e0
Alejandro Rojo <pinkyig@MacBook-Pro-de-Alejandro.local>
2024-06-13 13:30:11 -0400

    [Update] Se escribieron comentarios para explicar las funciones segun el formato de la tara

--- P1.rkt
+++ P1.rkt
@@ -1,16 +1,25 @@
 #lang scheme
 
+;; Descripción de la función
+;; La función `liberar` toma una lista de planetas con sus porcentajes de liberación y devuelve una lista que contiene el nombre del planeta con el menor porcentaje de liberación.
+;;
+;; lista : Lista de listas donde cada sublista contiene el nombre de un planeta y su porcentaje de liberación.
+
 (define (liberar lista)
 
-  (if(null? lista)
-     ;; Si se cumple el if 
-     '()
-     
-     ;; Si no se cumple el if
-     (let iteracion ((resto (cdr lista)) (menor (car lista)))
+  (if (null? lista)
+      ;; Si la lista está vacía, devuelve una lista vacía.
+      '()
+      
+      ;; Si la lista no está vacía, inicia la iteración.
+      (let iteracion ((resto (cdr lista)) (menor (car lista)))
+        ;; iteracion: Función auxiliar que recorre la lista para encontrar el planeta con menor porcentaje de liberación.
+        ;; resto: El resto de la lista de planetas por procesar.
+        ;; menor: El planeta con el menor porcentaje de liberación encontrado hasta ahora.
         (if (null? resto)
-            ;;Si el resto de la lista  esta vacio sigifica que se encontro el planeta con menor liberacion por que se se hace un car para obetener el nombre 
+
             (list (car menor))
+            
             
             (let* ((siguiente (car resto))
                    (nombre (car siguiente))
@@ -18,15 +27,15 @@
                    (menor-nombre (car menor))
                    (menor-porcentaje (cadr menor)))
               
+              ;; Compara el porcentaje actual con el menor porcentaje encontrado hasta ahora.
               (if (< porcentaje menor-porcentaje)
+                 
                   (iteracion (cdr resto) siguiente)
-                  (iteracion (cdr resto) menor))
-              
-              )))
-     )
+                  
+                  (iteracion (cdr resto) menor))))))
   )
-  
 
 ;; Ejemplos de uso:
 (display (liberar '(("Malevelon Creek" 50.0) ("Rend" 43.0154) ("Caladan" 100.0) ("Trantor" 68.59))))
+
 
